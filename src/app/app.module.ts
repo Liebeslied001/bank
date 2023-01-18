26e3e13fd6414947b993992fefe6c32d48fef813
mersaucedo <mersaucedosantos@gmail.com>
2023-01-18 18:43:16 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
 import { CreateNewCategoryComponent } from './components/create-new-category/create-new-category.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TransactionsComponent,
-    CreateNewCategoryComponent
+    CreateNewCategoryComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
